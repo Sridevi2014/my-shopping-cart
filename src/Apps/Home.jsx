c00ca54e78db1b4182280349e7877faa055f2105
Sridevi2014 <sridevips@hotmail.com>
2019-04-17 13:51:41 -0600

    new

--- src/Apps/Home.jsx
+++ src/Apps/Home.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { addToCart } from "./cart/cartActions";
-
+//class component
 class Home extends Component {
   //handleClick function
   handleClick = id => {
